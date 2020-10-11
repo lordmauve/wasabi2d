618bb54d91ffcc61a9d1e81c596d2009626cfe8a
Daniel Pope <mauve@mauveweb.co.uk>
2020-10-11 23:27:30 +0100

    Add cursor to coroutines.run example

--- examples/coroutines/run.py
+++ examples/coroutines/run.py
@@ -11,6 +11,15 @@ target = vec2(scene.width, scene.height) / 2
 scene.layers[0].set_effect('dropshadow', opacity=1, radius=1)
 
 
+cursor = scene.layers[0].add_polygon(
+    [(0, 0), (-15, 5), (-13, 0), (-15, -5)],
+    fill=False,
+    color='black',
+    stroke_width=3,
+)
+cursor.angle = 4
+
+
 async def enemy():
     color = colorsys.hsv_to_rgb(random.random(), 1, 1)
     pos = vec2(
@@ -35,10 +44,11 @@ async def enemy():
         pos += to_target.scaled_to(100 * dt)
         e.pos = pos
 
+    scene.camera.screen_shake()
     await animate(
         e,
-        duration=0.5,
-        scale=4,
+        duration=0.1,
+        scale=10,
         tween='accelerate',
         color=(*color, 0)
     )
@@ -48,6 +58,7 @@ async def enemy():
 @w2d.event
 def on_mouse_move(pos):
     global target
+    cursor.pos = pos
     target = vec2(pos)
 
 
